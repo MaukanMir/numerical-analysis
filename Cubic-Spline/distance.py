8ebfe724791248d877eb5a1aec0779c200347330
MaukanMir <Maukanm22@gmail.com>
2024-03-18 19:31:29 -0400

    Added all changes

--- Cubic-Spline/distance.py
+++ Cubic-Spline/distance.py
@@ -7,7 +7,6 @@ times = np.array([22.35, 45.73, 96.06, 121.57])  # in seconds
 
 # Fit a cubic spline to the data
 spline = CubicSpline(distances, times)
-
 # Predict the time at the 3/4 mile pole
 predicted_time_34_mile = spline(0.75)
 print(spline)
